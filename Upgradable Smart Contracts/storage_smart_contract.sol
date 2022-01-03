651e81b945d2e77d116195818ea30e06f8484479
Md. Maruf Hossain <69377574+md-maruf-hossain@users.noreply.github.com>
2022-01-03 21:54:24 +0600

    storage contract name change

--- Upgradable Smart Contracts/storage_smart_contract.sol
+++ Upgradable Smart Contracts/storage_smart_contract.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.11;
 
-contract Storage {
+contract myStorage {
     uint256 public myNumber;
     function setMyNumber(uint256 _myNumber) public {
         myNumber = _myNumber;
